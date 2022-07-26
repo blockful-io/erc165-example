beb92d5dd358ca9f18026fd41f51159540be944d
Gill <asguilift@hotmail.com>
2022-07-26 18:57:39 -0300

    Optimization

--- test/sample-test.js
+++ test/sample-test.js
@@ -7,24 +7,16 @@ describe("Greeter", function () {
     const contractName = "Greeter";
     const artifactPath = "contracts/"+contractName+".sol:/"+contractName;
     const abi = await hre.artifacts.readArtifact(artifactPath);
-
     // Concatenate function name with its param types
     const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
-
     // Encode function selector (assume web3 is globally available)
     const encodeSelector = f => ethers.utils.id(f).slice(0,10);
-
     // Parse ABI and encode its functions
     const output = abi["abi"]
       .filter(e => e.type === "function")
       .flatMap(e => `${encodeSelector(prepareData(e))}`);
       //.flatMap(e => `${encodeSelector(prepareData(e))}: ${prepareData(e)}`);
-
-    let xor = '';
-    for(let i = 0; i < output.length; i++){
-      xor = xor ^ output[i];
-    }
-    console.log(ethers.utils.hexlify(xor));
-
+    // Xor the output values and convert to hex
+    console.log(ethers.utils.hexlify(output.reduce((prev, cur) => prev ^ cur)));
   });
 });
